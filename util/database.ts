e7771cdae3ad222c4d4f184a99b56c5826bf3574
hailey ham <roserealslow@gmail.com>
2023-11-12 23:09:34 +0900

    (#2) global _mongo type 선언

--- util/database.ts
+++ util/database.ts
@@ -5,6 +5,10 @@ const url = config.mongodb_url;
 const options = { useNewUrlParser: true };
 let connectDB;
 
+if (!url) {
+  throw new Error('The MONGODB_URL environment variable is not defined');
+}
+
 if (process.env.NODE_ENV === 'development') {
   if (!global._mongo) {
     global._mongo = new MongoClient(url, options).connect();
