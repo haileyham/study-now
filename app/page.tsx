d8d6e4049bac3c3b32320d5e0ea7ec02755ce4f5
hailey ham <roserealslow@gmail.com>
2023-11-13 17:42:41 +0900

    Chore : 주소수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import styles from './page.module.scss';
-import Carousel from '@/components/carousel';
+import Carousel from '@/components/Carousel';
 
 export default function Home() {
   return (
