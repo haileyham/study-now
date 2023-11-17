edea34d86273fe10df30140a7275d9de7bd267b1
hailey ham <roserealslow@gmail.com>
2023-11-17 13:09:26 +0900

    (#6) style : 스터디목록페이지 container 및 main width 조절

--- app/study-post/page.tsx
+++ app/study-post/page.tsx
@@ -5,45 +5,47 @@ import Link from 'next/link';
 export default function StudyPost() {
   return (
     <>
-      <main className={styles.studyMain}>
-        <header className={styles.studyHeader}>
-          <div>
-            <h1>StudyList</h1>
-            <p>스터디 멤버를 구하세요!</p>
-          </div>
-          <Link href="/study-post/write">
-            <span>글쓰기</span>
-          </Link>
-        </header>
-        <section className={styles.studySection}>
-          <Link href="/study-post/detail/abc">
-            <div className={styles.studyList}>
-              <span className={styles.status}>모집중</span>
-              <span className={styles.date}>1일전</span>
-              <h2>스터디 모집해용</h2>
-              <p>우리 지금 해야하는 스터디는 이런 것입니다.</p>
-              <div className={styles.postInfo}>
-                <span>hailey</span>
-                <span>♡ 1</span>
-                <span>♧ 2</span>
-              </div>
+      <div className="container">
+        <main className={styles.studyMain}>
+          <header className={styles.studyHeader}>
+            <div>
+              <h1>StudyList</h1>
+              <p>스터디 멤버를 구하세요!</p>
             </div>
-          </Link>
-          <Link href="/study-post/detail/abc">
-            <div className={styles.studyList}>
-              <span className={styles.status}>모집중</span>
-              <span className={styles.date}>1일전</span>
-              <h2>스터디 모집해용</h2>
-              <p>우리 지금 해야하는 스터디는 이런 것입니다.</p>
-              <div className={styles.postInfo}>
-                <span>hailey</span>
-                <span>♡ 1</span>
-                <span>♧ 2</span>
+            <Link href="/study-post/write">
+              <span>글쓰기</span>
+            </Link>
+          </header>
+          <section className={styles.studySection}>
+            <Link href="/study-post/detail/abc">
+              <div className={styles.studyList}>
+                <span className={styles.status}>모집중</span>
+                <span className={styles.date}>1일전</span>
+                <h2>스터디 모집해용</h2>
+                <p>우리 지금 해야하는 스터디는 이런 것입니다.</p>
+                <div className={styles.postInfo}>
+                  <span>hailey</span>
+                  <span>♡ 1</span>
+                  <span>♧ 2</span>
+                </div>
               </div>
-            </div>
-          </Link>
-        </section>
-      </main>
+            </Link>
+            <Link href="/study-post/detail/abc">
+              <div className={styles.studyList}>
+                <span className={styles.status}>모집중</span>
+                <span className={styles.date}>1일전</span>
+                <h2>스터디 모집해용</h2>
+                <p>우리 지금 해야하는 스터디는 이런 것입니다.</p>
+                <div className={styles.postInfo}>
+                  <span>hailey</span>
+                  <span>♡ 1</span>
+                  <span>♧ 2</span>
+                </div>
+              </div>
+            </Link>
+          </section>
+        </main>
+      </div>
     </>
   );
 }
