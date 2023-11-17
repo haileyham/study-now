402b69493333b36ffdc5ecf4cdf003724cdf8096
hailey ham <roserealslow@gmail.com>
2023-11-17 13:24:55 +0900

    (#6) style : btn 공통처리 및 크기조정

--- app/study-post/page.tsx
+++ app/study-post/page.tsx
@@ -13,10 +13,49 @@ export default function StudyPost() {
               <p>스터디 멤버를 구하세요!</p>
             </div>
             <Link href="/study-post/write">
-              <span>글쓰기</span>
+              <span className="btn-s">글쓰기</span>
             </Link>
           </header>
           <section className={styles.studySection}>
+            <Link href="/study-post/detail/abc">
+              <div className={styles.studyList}>
+                <span className={styles.status}>모집중</span>
+                <span className={styles.date}>1일전</span>
+                <h2>스터디 모집해용</h2>
+                <p>우리 지금 해야하는 스터디는 이런 것입니다.</p>
+                <div className={styles.postInfo}>
+                  <span>hailey</span>
+                  <span>♡ 1</span>
+                  <span>♧ 2</span>
+                </div>
+              </div>
+            </Link>
+            <Link href="/study-post/detail/abc">
+              <div className={styles.studyList}>
+                <span className={styles.status}>모집중</span>
+                <span className={styles.date}>1일전</span>
+                <h2>스터디 모집해용</h2>
+                <p>우리 지금 해야하는 스터디는 이런 것입니다.</p>
+                <div className={styles.postInfo}>
+                  <span>hailey</span>
+                  <span>♡ 1</span>
+                  <span>♧ 2</span>
+                </div>
+              </div>
+            </Link>
+            <Link href="/study-post/detail/abc">
+              <div className={styles.studyList}>
+                <span className={styles.status}>모집중</span>
+                <span className={styles.date}>1일전</span>
+                <h2>스터디 모집해용</h2>
+                <p>우리 지금 해야하는 스터디는 이런 것입니다.</p>
+                <div className={styles.postInfo}>
+                  <span>hailey</span>
+                  <span>♡ 1</span>
+                  <span>♧ 2</span>
+                </div>
+              </div>
+            </Link>
             <Link href="/study-post/detail/abc">
               <div className={styles.studyList}>
                 <span className={styles.status}>모집중</span>
