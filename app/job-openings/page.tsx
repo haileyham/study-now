49eea6ab89de5b8292d2d7b40cf267b78e671eb4
hailey ham <roserealslow@gmail.com>
2023-11-18 13:57:21 +0900

    (#13) style : 채용정보페이지 마크업

--- app/job-openings/page.tsx
+++ app/job-openings/page.tsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import styles from './page.module.scss';
 
 export default function JobOpenings() {
-  return <div>page</div>;
+  return (
+    <>
+      <div className="container">
+        <main className={styles.main}>
+          <section className={styles.jobSearchSection}>
+            <h2 className="a11y-hidden">채용정보</h2>
+            <div className={styles.searchbox}>
+              <span>◎</span>
+              <input type="text" />
+              <span className={styles.searchClose}>Ⅹ</span>
+            </div>
+            <div>
+              <button></button>
+            </div>
+          </section>
+          <section className={styles.jobListSection}></section>
+        </main>
+      </div>
+    </>
+  );
 }
