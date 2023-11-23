20c6e3bcd17bf89121a40776d02143461ccdac33
hailey ham <roserealslow@gmail.com>
2023-11-23 13:28:22 +0900

    (#13) style:채용페이지-채용정보사이트 list

--- app/job-openings/page.tsx
+++ app/job-openings/page.tsx
@@ -86,6 +86,65 @@ export default function JobOpenings() {
               </li>
             </ul>
           </section>
+          <section className={styles.jobOpeningWebSite}>
+            <ul>
+              <li>
+                <div>
+                  <div className={styles.imgBox}>
+                    <img
+                      src="https://i.namu.wiki/i/hQbC1A-Bo2bhXpnTJd-Po8WKzT34w-9UN0xDYkUKOA-uV6rLOJUOVUlLw4jjwAyxx4gk4-qgcFxB4pmpcv_OE7U7PYsIo90vuMK6btYwE5M_rzQso7iqapRHFRyg7EjDyIXRfv7ZGwbqIRaxruvoIg.webp"
+                      alt="원티드"
+                    />
+                  </div>
+                  <p>채용사이트명</p>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <div className={styles.imgBox}>
+                    <img
+                      src="https://cdn.jumpit.co.kr/jumpit/jumpit_share.png"
+                      alt="점핏"
+                    />
+                  </div>
+                  <p>채용사이트명</p>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <div className={styles.imgBox}>
+                    <img
+                      src="https://jpassets.jobplanet.co.kr/production/uploads/material/media/7444/jp_og.png"
+                      alt="잡플레닛"
+                    />
+                  </div>
+                  <p>채용사이트명</p>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <div className={styles.imgBox}>
+                    <img
+                      src="https://oopy.lazyrockets.com/api/rest/cdn/image/0821aa81-6c17-485e-98b4-9589a62e7ae2.png"
+                      alt="잡코리아"
+                    />
+                  </div>
+                  <p>채용사이트명</p>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <div className={styles.imgBox}>
+                    <img
+                      src="https://i.namu.wiki/i/TQfS31XrpTACrWnhtrLZmGpYnn8ihiDPVSO6i5Iq91jSOZNxIsKOaGiz1Pa-BxpgJJ9xm7i7Ena92nOi7RPW9Ah15pcNv_FJ1KnKiPlxxYtj56b5_tGBYEccBYN42wpebo9JYxPc1O5mCUcq0qtWkg.svg"
+                      alt="사람인"
+                    />
+                  </div>
+                  <p>채용사이트명</p>
+                </div>
+              </li>
+            </ul>
+          </section>
         </main>
       </div>
     </>
