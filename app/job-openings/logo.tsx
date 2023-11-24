27b9cd2635d9ee14d0afdbc58135bc4afd776ff2
hailey ham <roserealslow@gmail.com>
2023-11-24 15:00:19 +0900

    (#13) style:채용페이지-사이트로고파트 코드분리

---
+++ app/job-openings/logo.tsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import styles from './page.module.scss';
+
+export default function SiteLogo() {
+  return (
+    <section className={styles.jobOpeningLogo}>
+      <div className={styles.logoContainer1}>
+        <div className={styles.imgBox}>
+          <img
+            src="https://i.namu.wiki/i/hQbC1A-Bo2bhXpnTJd-Po8WKzT34w-9UN0xDYkUKOA-uV6rLOJUOVUlLw4jjwAyxx4gk4-qgcFxB4pmpcv_OE7U7PYsIo90vuMK6btYwE5M_rzQso7iqapRHFRyg7EjDyIXRfv7ZGwbqIRaxruvoIg.webp"
+            alt="원티드"
+          />
+        </div>
+        <div className={styles.imgBox}>
+          <img
+            src="https://cdn.jumpit.co.kr/jumpit/jumpit_share.png"
+            alt="점핏"
+          />
+        </div>
+        <div className={styles.imgBox}>
+          <img
+            src="https://jpassets.jobplanet.co.kr/production/uploads/material/media/7444/jp_og.png"
+            alt="잡플레닛"
+          />
+        </div>
+        <div className={styles.imgBox}>
+          <img
+            src="https://oopy.lazyrockets.com/api/rest/cdn/image/0821aa81-6c17-485e-98b4-9589a62e7ae2.png"
+            alt="잡코리아"
+          />
+        </div>
+        <div className={styles.imgBox}>
+          <img
+            src="https://i.namu.wiki/i/TQfS31XrpTACrWnhtrLZmGpYnn8ihiDPVSO6i5Iq91jSOZNxIsKOaGiz1Pa-BxpgJJ9xm7i7Ena92nOi7RPW9Ah15pcNv_FJ1KnKiPlxxYtj56b5_tGBYEccBYN42wpebo9JYxPc1O5mCUcq0qtWkg.svg"
+            alt="사람인"
+          />
+        </div>
+      </div>
+      <div className={styles.logoContainer2}>
+        <div className={styles.imgBox}>
+          <img
+            src="https://i.namu.wiki/i/hQbC1A-Bo2bhXpnTJd-Po8WKzT34w-9UN0xDYkUKOA-uV6rLOJUOVUlLw4jjwAyxx4gk4-qgcFxB4pmpcv_OE7U7PYsIo90vuMK6btYwE5M_rzQso7iqapRHFRyg7EjDyIXRfv7ZGwbqIRaxruvoIg.webp"
+            alt="원티드"
+          />
+        </div>
+        <div className={styles.imgBox}>
+          <img
+            src="https://cdn.jumpit.co.kr/jumpit/jumpit_share.png"
+            alt="점핏"
+          />
+        </div>
+        <div className={styles.imgBox}>
+          <img
+            src="https://jpassets.jobplanet.co.kr/production/uploads/material/media/7444/jp_og.png"
+            alt="잡플레닛"
+          />
+        </div>
+        <div className={styles.imgBox}>
+          <img
+            src="https://oopy.lazyrockets.com/api/rest/cdn/image/0821aa81-6c17-485e-98b4-9589a62e7ae2.png"
+            alt="잡코리아"
+          />
+        </div>
+        <div className={styles.imgBox}>
+          <img
+            src="https://i.namu.wiki/i/TQfS31XrpTACrWnhtrLZmGpYnn8ihiDPVSO6i5Iq91jSOZNxIsKOaGiz1Pa-BxpgJJ9xm7i7Ena92nOi7RPW9Ah15pcNv_FJ1KnKiPlxxYtj56b5_tGBYEccBYN42wpebo9JYxPc1O5mCUcq0qtWkg.svg"
+            alt="사람인"
+          />
+        </div>
+      </div>
+      <div className={styles.logoContainer3}>
+        <div className={styles.imgBox}>
+          <img
+            src="https://i.namu.wiki/i/hQbC1A-Bo2bhXpnTJd-Po8WKzT34w-9UN0xDYkUKOA-uV6rLOJUOVUlLw4jjwAyxx4gk4-qgcFxB4pmpcv_OE7U7PYsIo90vuMK6btYwE5M_rzQso7iqapRHFRyg7EjDyIXRfv7ZGwbqIRaxruvoIg.webp"
+            alt="원티드"
+          />
+        </div>
+        <div className={styles.imgBox}>
+          <img
+            src="https://cdn.jumpit.co.kr/jumpit/jumpit_share.png"
+            alt="점핏"
+          />
+        </div>
+        <div className={styles.imgBox}>
+          <img
+            src="https://jpassets.jobplanet.co.kr/production/uploads/material/media/7444/jp_og.png"
+            alt="잡플레닛"
+          />
+        </div>
+        <div className={styles.imgBox}>
+          <img
+            src="https://oopy.lazyrockets.com/api/rest/cdn/image/0821aa81-6c17-485e-98b4-9589a62e7ae2.png"
+            alt="잡코리아"
+          />
+        </div>
+        <div className={styles.imgBox}>
+          <img
+            src="https://i.namu.wiki/i/TQfS31XrpTACrWnhtrLZmGpYnn8ihiDPVSO6i5Iq91jSOZNxIsKOaGiz1Pa-BxpgJJ9xm7i7Ena92nOi7RPW9Ah15pcNv_FJ1KnKiPlxxYtj56b5_tGBYEccBYN42wpebo9JYxPc1O5mCUcq0qtWkg.svg"
+            alt="사람인"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
