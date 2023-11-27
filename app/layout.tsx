bcb758dd6a8bbf769cc373074484674808b83029
hailey ham <roserealslow@gmail.com>
2023-11-27 23:04:35 +0900

    (#5#10) feat:nav 로그인 버튼 컴포넌트

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.scss';
 import Link from 'next/link';
+import { LoginBtn } from '@/components/LoginOutBtn';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
           <div className="myContainer">
             <div>🌛</div>
             <div>🎃</div>
+            <LoginBtn></LoginBtn>
           </div>
           <ul className="navContainer">
             <Link href="/">
