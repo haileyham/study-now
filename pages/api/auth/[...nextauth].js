cf9bd3213533e3a4d85812e07fb15924a20fc429
hailey ham <roserealslow@gmail.com>
2023-11-27 23:02:13 +0900

    (#5) feat : 회원가입-nextauth 소셜로그인 카카오,깃헙(환경변수o)

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import KakaoProvider from "next-auth/providers/kakao";
+
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_ID,
+    }),
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET,
+    })
+  ],
+  secret: process.env.NEXTAUTH_JWT_SECRET
+};
+export default NextAuth(authOptions); 
