9dbda381449752c92ec6c8ced2ff6526c2e69937
hailey ham <roserealslow@gmail.com>
2023-11-30 18:27:20 +0900

    (#29) refac : 글작성서버-빈칸예외처리/body추가/에러처리

--- pages/api/post/new.ts
+++ pages/api/post/new.ts
@@ -5,11 +5,31 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  let save = {
-    title: req.body.study.title,
-    content: req.body.study.content,
-  };
-  const db = (await connectDB).db('study_platform');
-  let result = await db.collection('study_posts').insertOne(save);
-  return res.status(201).json('정상');
+  if (req.method === 'POST') {
+    if (
+      req.body.study.author == '' ||
+      req.body.study.title == '' ||
+      req.body.study.content == '' ||
+      req.body.study.status == ''
+    ) {
+      return res.status(400).json('빈칸안됑');
+    }
+    try {
+      let save = {
+        author: req.body.study.author,
+        title: req.body.study.title,
+        content: req.body.study.content,
+        status: req.body.study.status,
+        mode: req.body.study.mode,
+        location: req.body.study.location,
+        time: req.body.study.time,
+        type: req.body.study.type,
+      };
+      const db = (await connectDB).db('study_platform');
+      let result = await db.collection('study_posts').insertOne(save);
+      return res.status(201).json('정상');
+    } catch (error) {
+      return res.status(500).json('서버 에러 발생');
+    }
+  }
 }
