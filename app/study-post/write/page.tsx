276164046a8c335fa6949ef722090290a4a0ab5c
hailey ham <roserealslow@gmail.com>
2023-11-30 18:29:43 +0900

    (#7) refac: 글작성페이지 author 정보추가 - 추후props넘겨받기

--- app/study-post/write/page.tsx
+++ app/study-post/write/page.tsx
@@ -5,6 +5,7 @@ import styles from './page.module.scss';
 
 export default function StudyWrite() {
   const [studyWrite, setStudyWrite] = useState<StudyWriteState>({
+    author: '',
     status: '',
     mode: '',
     location: '',
