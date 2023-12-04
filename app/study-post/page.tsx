60f8f94138ffec041fb844672d68d14a9009353c
hailey ham <roserealslow@gmail.com>
2023-12-04 19:58:24 +0900

    (#6) refac : 스터디게시판-list 컴포넌트 분리 및 props type처리

--- app/study-post/page.tsx
+++ app/study-post/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './page.module.scss';
 import Link from 'next/link';
 import { connectDB } from '@/util/database';
+import PostList from './postList';
 
 // export const revalidate = 60;
 export const dynamic = 'force-dynamic';
@@ -9,6 +10,10 @@ export const dynamic = 'force-dynamic';
 export default async function StudyPost() {
   const db = (await connectDB).db('study_platform');
   let result: Post[] = await db.collection('study_posts').find().toArray();
+  result.map((post) => {
+    post._id = post._id.toString();
+    return post;
+  });
 
   return (
     <>
@@ -24,53 +29,7 @@ export default async function StudyPost() {
             </Link>
           </header>
           <section className={styles.studySection}>
-            {result.map((post: Post, i: number) => {
-              return (
-                <div key={i}>
-                  <Link href={`/study-post/detail/${post._id}`}>
-                    <div className={styles.studyList}>
-                      {/* <span
-                        className={
-                          post.status == 'recruiting'
-                            ? styles.statusRR
-                            : styles.statusDD
-                        }
-                      ></span> */}
-                      <span
-                        className={
-                          post.status == 'recruiting'
-                            ? styles.statusR
-                            : styles.statusD
-                        }
-                      >
-                        {post.status == 'recruiting' ? '모집중' : '모집완료'}
-                      </span>
-                      <span className={styles.date}>1일전</span>
-                      <h2>{post.title}</h2>
-                      <p>{post.content}</p>
-                      <div className={styles.postInfo}>
-                        {post.mode && (
-                          <span className={styles.info1}>{post.mode}</span>
-                        )}
-                        {post.location && (
-                          <span className={styles.info2}>{post.location}</span>
-                        )}
-                        {post.time && (
-                          <span className={styles.info3}>{post.time}</span>
-                        )}
-                        {post.type && (
-                          <span className={styles.info4}>{post.type}</span>
-                        )}
-
-                        <span>작성자 : Zoe</span>
-                        {/* <span>♡ 1</span> */}
-                        {/* <span>♧ 2</span> */}
-                      </div>
-                    </div>
-                  </Link>
-                </div>
-              );
-            })}
+            <PostList result={result}></PostList>
           </section>
         </main>
       </div>
