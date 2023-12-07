5f2e650cd8d67a204ba61137a711356e9104f7b8
hailey ham <roserealslow@gmail.com>
2023-12-07 11:46:17 +0900

    (#35) refac:글삭제후 result 에러값 / if조건으로 랜더링 / 삭제된페이지 안내

--- app/study-post/detail/[id]/page.tsx
+++ app/study-post/detail/[id]/page.tsx
@@ -9,74 +9,87 @@ export default async function StudyDetail(props: StudyDetailProps) {
   let result = await db
     .collection('study_posts')
     .findOne({ _id: new ObjectId(props.params.id) });
+  let id;
+  if (result) {
+    id = result._id.toString();
+  } else {
+    console.error('Study post not found');
+    // 또는 에러 처리 로직을 추가할 수 있습니다.
+  }
 
-  return (
-    <>
-      <div className={`container ${styles.container}`}>
-        <header>
-          <h1 className="a11y-hidden">스터디 모집 상세 페이지</h1>
-        </header>
-        <main className={styles.main}>
-          <section>
-            <header className={styles.mainHeader}>
-              <div>
-                <h2>{result.title}</h2>
-                <div className={styles.detailEditDel}>
-                  <button className="btn-s">글수정</button>
-                  {/* <button className="btn-s">글삭제</button> */}
-                  <DeleteBtn postId={result._id}></DeleteBtn>
-                </div>
-              </div>
-              <div>
-                <img src="" alt="profile" />
-                <p>author</p>
-                <p className={styles.date}>1일전</p>
-              </div>
-            </header>
-            <div className={styles.mainContent}>
-              <div className={styles.postInfo}>
+  if (result) {
+    return (
+      <>
+        <div className={`container ${styles.container}`}>
+          <header>
+            <h1 className="a11y-hidden">스터디 모집 상세 페이지</h1>
+          </header>
+          <main className={styles.main}>
+            <section>
+              <header className={styles.mainHeader}>
                 <div>
-                  <span>status</span>
-                  <p>{result.status == 'recruiting' ? '모집중' : '모집완료'}</p>
+                  <h2>{result.title}</h2>
+                  <div className={styles.detailEditDel}>
+                    <button className="btn-s">글수정</button>
+                    {/* <button className="btn-s">글삭제</button> */}
+                    <DeleteBtn postId={id}></DeleteBtn>
+                  </div>
                 </div>
                 <div>
-                  <span>mode</span>
-                  <p>{result.mode}</p>
-                </div>
-                <div>
-                  <span>Location</span>
-                  <p>{result.location}</p>
-                </div>
-                <div>
-                  <span>Type</span>
-                  <p>{result.type}</p>
-                </div>
-              </div>
-              <p>{result.content}</p>
-            </div>
-          </section>
-          <section>
-            <h3>Comment</h3>
-            <ul className={styles.commentWrapper}>
-              <li>
-                <div className={styles.commnetInfo}>
                   <img src="" alt="profile" />
                   <p>author</p>
+                  <p className={styles.date}>1일전</p>
                 </div>
-                <p className={styles.comment}>
-                  댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다l
-                  댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다l
-                </p>
-                <p className={styles.commentDate}>1일전</p>
-              </li>
-            </ul>
-            <div className={styles.commentInput}>
-              <input type="text" placeholder="댓글을 남겨주세요" />
-              <button className="btn-s">전송</button>
-            </div>
-          </section>
-        </main>
-      </div>
-    </>
-  );
+              </header>
+              <div className={styles.mainContent}>
+                <div className={styles.postInfo}>
+                  <div>
+                    <span>status</span>
+                    <p>
+                      {result.status == 'recruiting' ? '모집중' : '모집완료'}
+                    </p>
+                  </div>
+                  <div>
+                    <span>mode</span>
+                    <p>{result.mode}</p>
+                  </div>
+                  <div>
+                    <span>Location</span>
+                    <p>{result.location}</p>
+                  </div>
+                  <div>
+                    <span>Type</span>
+                    <p>{result.type}</p>
+                  </div>
+                </div>
+                <p>{result.content}</p>
+              </div>
+            </section>
+            <section>
+              <h3>Comment</h3>
+              <ul className={styles.commentWrapper}>
+                <li>
+                  <div className={styles.commnetInfo}>
+                    <img src="" alt="profile" />
+                    <p>author</p>
+                  </div>
+                  <p className={styles.comment}>
+                    댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다l
+                    댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다댓글입니다l
+                  </p>
+                  <p className={styles.commentDate}>1일전</p>
+                </li>
+              </ul>
+              <div className={styles.commentInput}>
+                <input type="text" placeholder="댓글을 남겨주세요" />
+                <button className="btn-s">전송</button>
+              </div>
+            </section>
+          </main>
+        </div>
+      </>
+    );
+  } else {
+    return <>게시물삭제</>;
+  }
 }
