2d0d85905c924a44fb8999ddf5c324e7d4c81955
hailey ham <roserealslow@gmail.com>
2023-12-08 11:16:06 +0900

    (#7, #6) feat : 글작성시간 표시(분,시,일)

--- app/study-post/postList.tsx
+++ app/study-post/postList.tsx
@@ -3,9 +3,32 @@ import styles from './page.module.scss';
 import Link from 'next/link';
 
 const PostList: React.FC<PostListProps> = ({ result }) => {
+  //시간
+  const getTimesAgo = (postDate: string) => {
+    const postDateTime = new Date(postDate);
+    const currentDateTime = new Date();
+
+    const timeDifference = currentDateTime.getTime() - postDateTime.getTime();
+    const minutesAgo = Math.floor(timeDifference / (1000 * 60));
+    const hoursAgo = Math.floor(timeDifference / (1000 * 60 * 60));
+    const daysAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+    if (minutesAgo < 1) {
+      return '방금 전';
+    }
+    if (minutesAgo < 60) {
+      return `${minutesAgo}분 전`;
+    }
+    if (hoursAgo < 24) {
+      return `${hoursAgo}시간 전`;
+    }
+    return `${daysAgo}일 전`;
+  };
+
   return (
     <div>
       {result.map((post: Post, i: number) => {
+        console.log(post);
         return (
           <div key={i}>
             <Link href={`/study-post/detail/${post._id}`}>
@@ -26,7 +49,7 @@ const PostList: React.FC<PostListProps> = ({ result }) => {
                 >
                   {post.status == 'recruiting' ? '모집중' : '모집완료'}
                 </span>
-                <span className={styles.date}>1일전</span>
+                <span className={styles.date}>{getTimesAgo(post.date)}</span>
                 <h2>{post.title}</h2>
                 <p>{post.content}</p>
                 <div className={styles.postInfo}>
