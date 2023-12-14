ab9bd93722593f46c7b9076225578f4af4b9fc86
hailey ham <roserealslow@gmail.com>
2023-12-14 21:24:23 +0900

    (#8) feat : 수정페이지 - dynamic routing props 연결 및 db

--- app/study-post/edit/[id]/page.tsx
+++ app/study-post/edit/[id]/page.tsx
@@ -1,5 +1,11 @@
+import { connectDB } from '@/util/database';
+import { ObjectId } from 'mongodb';
 import React from 'react';
 
-export default function StudyEdit() {
-  return <div>StudyEdit</div>;
+export default async function StudyEdit(props: StudyDetailProps) {
+  const db = (await connectDB).db('study_platform');
+  const result = await db
+    .collection('study_posts')
+    .findOne({ _id: new ObjectId(props.params.id) });
+  return <div>{result.title}</div>;
 }
