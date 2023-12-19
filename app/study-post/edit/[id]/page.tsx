015b46e766b0b845d7bf1ec1f64cbe6ab103b8b9
hailey ham <roserealslow@gmail.com>
2023-12-19 12:50:56 +0900

    (#8) refac : 수정페이지 - DB 데이터 타입

--- app/study-post/edit/[id]/page.tsx
+++ app/study-post/edit/[id]/page.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 export default async function StudyEdit(props: StudyDetailProps) {
   const db = (await connectDB).db('study_platform');
-  const result = await db
+  const result: Post = await db
     .collection('study_posts')
     .findOne({ _id: new ObjectId(props.params.id) });
   result._id = result._id.toString();
