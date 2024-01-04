aaf4485ee8d4a6fad2dcb1f56568b8e6769b4397
hailey ham <roserealslow@gmail.com>
2024-01-04 18:25:02 +0900

    (#11) feat:퀴즈페이지-문제여러개출력

--- components/quiz/QuizComponent.tsx
+++ components/quiz/QuizComponent.tsx
@@ -3,19 +3,49 @@ import React, { useState } from 'react';
 import styles from './page.module.scss';
 
 export default function QuizComponent() {
+  const quizData = [
+    {
+      id: 1,
+      question: '블라블라Q. 블라블라Q. 블라블라Q. 블라블라Q. 블라블라Q.',
+      answer: 'hello',
+    },
+    {
+      id: 2,
+      question: '블루블루 블루짱',
+      answer: 'hello world',
+    },
+    {
+      id: 3,
+      question: '블라블라큐큐큐',
+      answer: 'hello hi',
+    },
+  ];
+
   const [input, setInput] = useState('');
-  const [answer, setAnswer] = useState('');
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [userAnswers, setUserAnswers] = useState(
+    Array(quizData.length).fill(''),
+  );
 
   const handleInputChange = (e: any) => {
-    setInput(e.target.value);
+    // setInput(e.target.value);
+    const updatedUserAnswers = [...userAnswers];
+    updatedUserAnswers[currentQuestion] = e.target.value;
+    setUserAnswers(updatedUserAnswers);
   };
 
-  const handleButtonClick = () => {
-    setAnswer(input);
-  };
+  // const handleButtonClick = () => {
+  //   setAnswer(input);
+  // };
 
   const handleNextButtonClick = () => {
-    localStorage.setItem('answer', answer);
+    // localStorage.setItem('answer', answer);
+    if (currentQuestion < quizData.length - 1) {
+      setCurrentQuestion((prevQuestion) => prevQuestion + 1);
+      setInput('');
+    } else {
+      localStorage.setItem('userAnswers', JSON.stringify(userAnswers));
+    }
   };
 
   return (
@@ -35,23 +65,27 @@ export default function QuizComponent() {
             </div>
           </section>
           <section className={styles.quiz}>
-            <p>
+            {/* <p>
               Q. 블라블라Q. 블라블라Q. 블라블라Q. 블라블라Q. 블라블라Q.
               블라블라Q. 블라블라Q. 블라블라Q. 블라블라Q. 블라블라Q. 블라블라Q.
               블라블라Q. 블라블라Q. 블라블라Q. 블라블라Q. 블라블라Q. 블라블라Q.
               블라블라Q. 블라블라Q. 블라블라Q. 블라블라
-            </p>
-            <span className={styles.answer}>{answer}</span>
+            </p> */}
+            <p>{quizData[currentQuestion].question}</p>
+            {/* <span className={styles.answer}>{answer}</span> */}
+            <span className={styles.answer}>
+              {userAnswers[currentQuestion]}
+            </span>
             <input type="text" onChange={handleInputChange} />
-            <button className="btn-s" onClick={handleButtonClick}>
+            {/* <button className="btn-s" onClick={handleButtonClick}>
               제출
-            </button>
+            </button> */}
             <button
-              className={
-                answer
-                  ? `btn-s ${styles.nextBtn} ${styles.nextBtnActive}`
-                  : `btn-s ${styles.nextBtn}`
-              }
+              // className={
+              //   answer
+              //     ? `btn-s ${styles.nextBtn} ${styles.nextBtnActive}`
+              //     : `btn-s ${styles.nextBtn}`
+              // }
               onClick={handleNextButtonClick}
             >
               Next ▶
