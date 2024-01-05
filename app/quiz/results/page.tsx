2dd1582a255159e24ae0548b4edae30fa7b8556e
hailey ham <roserealslow@gmail.com>
2024-01-05 14:58:16 +0900

    (#48) feat : 퀴즈결과페이지 생성 및 컴포넌트 연결

---
+++ app/quiz/results/page.tsx
@@ -0,0 +1,10 @@
+import QuizResults from '@/components/quiz/QuizResults';
+import React from 'react';
+
+export default function page() {
+  return (
+    <>
+      <QuizResults></QuizResults>
+    </>
+  );
+}
