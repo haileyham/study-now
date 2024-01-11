28639b224b27caa78b87dc8a53596cf7e1007f23
hailey ham <roserealslow@gmail.com>
2024-01-12 00:39:01 +0900

    (#11) feat : 퀴즈페이지- user 퀴즈 설정값 input 입력 및 quiz play페이지 라우팅

--- components/quiz/QuizSetting.tsx
+++ components/quiz/QuizSetting.tsx
@@ -2,17 +2,41 @@
 
 import React, { useState } from 'react';
 import QuizComponent from './QuizComponent';
+import { useRouter } from 'next/navigation';
 
 export default function QuizSetting() {
   const [questionNum, setQuestionNum] = useState(10);
   const [timerDuration, setTimerDuration] = useState(10);
+  const router = useRouter();
+
+  const handleSettingQuiz = (e: any) => {
+    const { name, value } = e.target; // Get input name and value
+    if (name === 'questionNum') {
+      setQuestionNum(parseInt(value)); // Parse value to number for questionNum
+    } else if (name === 'timerDuration') {
+      setTimerDuration(parseInt(value)); // Parse value to number for timerDuration
+    }
+  };
+
+  const handleMoveQuiz = () => {
+    router.push('/quiz/play');
+  };
 
   return (
     <>
-      <QuizComponent
-        questionNum={questionNum}
-        timerDuration={timerDuration}
-      ></QuizComponent>
+      <input
+        type="number"
+        name="questionNum"
+        defaultValue="10"
+        onChange={handleSettingQuiz}
+      />
+      <input
+        type="number"
+        name="timerDuration"
+        defaultValue="10"
+        onChange={handleSettingQuiz}
+      />
+      <button onClick={handleMoveQuiz}>퀴즈 풀기</button>
     </>
   );
 }
