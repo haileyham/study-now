a730dfbb9e4b30743c7f056b92d4ec97d9e27e0a
hailey ham <roserealslow@gmail.com>
2024-01-14 19:42:21 +0900

    (#11) refac : 퀴즈셋팅페이지 - 문제유형 모듈화

---
+++ components/quiz/QuizTypeSelect.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import React from 'react';
+import styles from './page.module.scss';
+
+interface QuizTypeSelectProps {
+  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+}
+
+const QuizTypeSelect: React.FC<QuizTypeSelectProps> = ({ onChange }) => {
+  return (
+    <div className={styles.quizType}>
+      <label htmlFor="quizType">문제유형</label>
+      <select name="문제유형" id="quizType" onChange={onChange}>
+        <option value="프론트엔드">프론트엔드</option>
+        <option value="CS">CS</option>
+      </select>
+    </div>
+  );
+};
+
+export default QuizTypeSelect;
