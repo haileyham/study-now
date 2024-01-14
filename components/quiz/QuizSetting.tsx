21e52ad81a073359a532ddd84df40d1e800cb5a6
hailey ham <roserealslow@gmail.com>
2024-01-14 19:04:47 +0900

    (#11) feat : 퀴즈셋팅페이지 - 문제유형 선택

--- components/quiz/QuizSetting.tsx
+++ components/quiz/QuizSetting.tsx
@@ -7,6 +7,7 @@ import { useRouter } from 'next/navigation';
 export default function QuizSetting() {
   const [questionNum, setQuestionNum] = useState(10);
   const [timerDuration, setTimerDuration] = useState(10);
+  const [quizType, setQuizType] = useState('프론트엔드');
   const router = useRouter();
 
   const handleIncrease = (name: string) => {
@@ -29,39 +30,54 @@ export default function QuizSetting() {
     router.push(`/quiz/play?q=${questionNum}&t=${timerDuration}`);
   };
 
+  const handleQuizTypeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setQuizType(e.target.value);
+  };
+
   return (
     <>
       <main className={styles.main}>
-        <button
-          onClick={() => {
-            handleDecrease('questionNum');
-          }}
-        >
-          -
-        </button>
-        <span>{questionNum}</span>
-        <button
-          onClick={() => {
-            handleIncrease('questionNum');
-          }}
-        >
-          +
-        </button>
-        <button
-          onClick={() => {
-            handleDecrease('timerDuration');
-          }}
-        >
-          -
-        </button>
-        <span>{timerDuration}</span>
-        <button
-          onClick={() => {
-            handleIncrease('timerDuration');
-          }}
-        >
-          +
-        </button>
+        <div className={styles.quizType}>
+          <label htmlFor="quizType">문제유형</label>
+          <select name="문제유형" id="quizType" onChange={handleQuizTypeChange}>
+            <option value="프론트엔드">프론트엔드</option>
+            <option value="CS">CS</option>
+          </select>
+        </div>
+        <div className={styles.questionNum}>
+          <button
+            onClick={() => {
+              handleDecrease('questionNum');
+            }}
+          >
+            -
+          </button>
+          <span>{questionNum}</span>
+          <button
+            onClick={() => {
+              handleIncrease('questionNum');
+            }}
+          >
+            +
+          </button>
+        </div>
+        <div className={styles.timerDuration}>
+          <button
+            onClick={() => {
+              handleDecrease('timerDuration');
+            }}
+          >
+            -
+          </button>
+          <span>{timerDuration}</span>
+          <button
+            onClick={() => {
+              handleIncrease('timerDuration');
+            }}
+          >
+            +
+          </button>
+        </div>
         <button onClick={handleMoveQuiz}>퀴즈 풀기</button>
       </main>
     </>
