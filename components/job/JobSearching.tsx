3d9e2c8cf938494e865c7833f84037ba5c46a91e
hailey ham <roserealslow@gmail.com>
2024-03-13 17:10:31 +0900

    (#85) feat : 채용페이지-검색버튼 추가 및 design

--- components/job/JobSearching.tsx
+++ components/job/JobSearching.tsx
@@ -1,3 +1,4 @@
+import SearchingBtn from '../common/SearchingBtn';
 import extractDistrict from './extractDistrict';
 import styles from './page.module.scss';
 import React, { ChangeEvent } from 'react';
@@ -5,32 +6,41 @@ import React, { ChangeEvent } from 'react';
 interface JobSearchingProps {
   onFunction?: (e: ChangeEvent<HTMLInputElement>) => void;
   result?: any;
+  onBtn?: () => void;
 }
 
 interface JobAPI {
   [key: string]: string;
 }
 
-const JobSearching: React.FC<JobSearchingProps> = ({ onFunction, result }) => {
+const JobSearching: React.FC<JobSearchingProps> = ({
+  onFunction,
+  result,
+  onBtn,
+}) => {
   return (
     <section className={styles.jobSearchSection}>
       <h2 className="a11y-hidden">채용정보</h2>
       <div className={styles.searchbox}>
-        <span>◎</span>
-        <input type="text" onChange={onFunction} />
-        <span className={styles.searchClose}>Ⅹ</span>
-        <ul>
-          {result?.map((company: JobAPI, i: number) => {
-            const match = extractDistrict(company.WORK_PARAR_BASS_ADRES_CN);
-            return (
-              <li className={styles.searchSelected} key={i}>
-                <p>{company.CMPNY_NM}</p>
-                <p>{match}</p>
-              </li>
-            );
-          })}
-        </ul>
-        <button className="btn-s">검색</button>
+        <div className={styles.searchIp}>
+          <input type="text" onChange={onFunction} />
+          <span className={styles.searchClose}>Ⅹ</span>
+          <ul>
+            {result?.map((company: JobAPI, i: number) => {
+              const match = extractDistrict(company.WORK_PARAR_BASS_ADRES_CN);
+              return (
+                <li className={styles.searchSelected} key={i}>
+                  <p>{company.CMPNY_NM}</p>
+                  <p>{match}</p>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <SearchingBtn
+          onFunction={onBtn}
+          style={styles.searchingBtn}
+        ></SearchingBtn>
       </div>
     </section>
   );
