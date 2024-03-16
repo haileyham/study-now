eb149e69a1f81657753e1d7a9807705f12072661
hailey ham <roserealslow@gmail.com>
2024-03-17 01:36:06 +0900

    (#85) fix : mixedContent

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  rewrites: () => [
+    {
+      source: "/app/job-openings",
+      destination: "http://study-now-pink.vercel.app/job-openings",
+    },
+  ],
+};
