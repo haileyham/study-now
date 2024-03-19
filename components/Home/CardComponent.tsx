c144751b88a1104b71562bf666a9563db1178150
hailey ham <roserealslow@gmail.com>
2024-03-19 15:34:08 +0900

    (#3) feat: 메인페이지-link추가 및 props추가

--- components/Home/CardComponent.tsx
+++ components/Home/CardComponent.tsx
@@ -1,22 +1,36 @@
+import Link from 'next/link';
 import styles from './page.module.scss';
 import React from 'react';
 
 interface CardComponentProps {
   imageUrl: string;
   title: string;
-  content: string;
+  url: string;
+  content1?: string;
+  content2?: string;
   style?: string;
 }
 
 export default function CardComponent({
   imageUrl,
   title,
-  content,
+  url,
+  content1,
+  content2,
   style,
 }: CardComponentProps) {
   return (
-    <div className={styles.card}>
-      <span>인기 Quiz</span>
-    </div>
+    <>
+      <div className={styles.card}>
+        <Link href={url}>
+          <div>
+            <h2>{title}</h2>
+            <p>{content1}</p>
+            <p>{content2}</p>
+          </div>
+          {/* <img src={imageUrl} alt="" /> */}
+        </Link>
+      </div>
+    </>
   );
 }
