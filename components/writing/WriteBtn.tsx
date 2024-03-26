c5ce489cedba1dbfb3b5df35f4688fa8f6bafbbc
hailey ham <roserealslow@gmail.com>
2024-03-26 23:43:19 +0900

    (#99) refac : 스터디목록글쓰기 버튼-로그인여부에 따른 modal 설정 및 접근

--- components/writing/WriteBtn.tsx
+++ components/writing/WriteBtn.tsx
@@ -3,21 +3,35 @@
 import React from 'react';
 import Modal from '../modal/Modal';
 import { useRouter } from 'next/navigation';
+import { signIn } from 'next-auth/react';
 
-export default function WriteBtn() {
+export default function WriteBtn({ session }: any) {
   let router = useRouter();
   const handleWrite = () => {
     router.push(`/study-post/write`);
   };
+  const handleLogin = () => {
+    signIn();
+  };
 
   return (
     <>
-      <Modal
-        modalBtn={`글쓰기`}
-        modalBtnStyle={`btn-s`}
-        message={`글을 작성하시겠습니까?`}
-        onFunction={handleWrite}
-      ></Modal>
+      {session ? (
+        <Modal
+          modalBtn={`글쓰기`}
+          modalBtnStyle={`btn-s`}
+          message={`글을 작성하시겠습니까?`}
+          onFunction={handleWrite}
+        ></Modal>
+      ) : (
+        <Modal
+          modalBtn={`글쓰기`}
+          modalBtnStyle={`btn-s`}
+          message={`로그인 필요`}
+          onFunction={handleLogin}
+          mark={`!`}
+        ></Modal>
+      )}
     </>
   );
 }
