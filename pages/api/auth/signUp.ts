cb9f289012678f36f09f557b608ec4baeb032d3d
hailey ham <roserealslow@gmail.com>
2024-03-27 16:26:38 +0900

    (#5) refac : 회원가입 서버 - POST 요청만 가능하도록&각 빈칸 예외처리

--- pages/api/auth/signUp.ts
+++ pages/api/auth/signUp.ts
@@ -6,18 +6,31 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  try {
-    let userInfo = {
-      name: req.body.info.name,
-      email: req.body.info.email,
-      pw: req.body.info.pw,
-    };
-    const hash = await bcrypt.hash(req.body.info.pw, 10);
-    req.body.info.pw = hash;
-    const db = (await connectDB).db('study_platform');
-    await db.collection('user_cred').insertOne(userInfo);
-    return res.status(201).json('정상 요청');
-  } catch (error) {
-    return res.status(500).json('server error');
+  if (req.method === 'POST') {
+    if (req.body.info.name == '') {
+      return res.status(400).json('이름을 입력해 주세요');
+    }
+    if (req.body.info.email == '') {
+      return res.status(400).json('이메일을 입력해 주세요');
+    }
+    if (req.body.info.pw == '') {
+      return res.status(400).json('비밀번호를 입력해 주세요');
+    }
+    try {
+      let userInfo = {
+        name: req.body.info.name,
+        email: req.body.info.email,
+        pw: req.body.info.pw,
+      };
+      const hash = await bcrypt.hash(req.body.info.pw, 10);
+      req.body.info.pw = hash;
+      const db = (await connectDB).db('study_platform');
+      await db.collection('user_cred').insertOne(userInfo);
+      return res.status(201).json('정상 요청');
+    } catch (error) {
+      return res.status(500).json('server error');
+    }
+  } else {
+    return res.status(400).json({ error: 'POST만 가능' });
   }
 }
