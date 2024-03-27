25bb3ffd5908c26e97d2cfad9a76d83d11717af6
hailey ham <roserealslow@gmail.com>
2024-03-27 14:42:07 +0900

    (#5) feat : 회원가입페이지-input 입력값

--- components/signUp/SignUp.tsx
+++ components/signUp/SignUp.tsx
@@ -1,6 +1,19 @@
+'use client';
+import { ChangeEvent, useState } from 'react';
 import styles from './page.module.scss';
 
 export default function SignUp() {
+  const [signUpInfo, setSignUpInfo] = useState({
+    name: '',
+    email: '',
+    pw: '',
+  });
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setSignUpInfo({ ...signUpInfo, [name]: value });
+  };
+  console.log(signUpInfo);
+
   return (
     <>
       <main className={styles.main}>
@@ -9,9 +22,24 @@ export default function SignUp() {
             <h2>Sign Up</h2>
           </header>
           <div>
-            <input type="text" placeholder="name" name="name" />
-            <input type="text" placeholder="example@example.com" name="email" />
-            <input type="password" placeholder="password" name="password" />
+            <input
+              type="text"
+              placeholder="name"
+              name="name"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              placeholder="example@example.com"
+              name="email"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              placeholder="password"
+              name="password"
+              onChange={handleChange}
+            />
             <button className="btn-m">Sign Up</button>
           </div>
         </section>
