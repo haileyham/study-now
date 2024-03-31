16208375759723a1a814b908610681b6494a5713
hailey ham <roserealslow@gmail.com>
2024-03-31 11:55:29 +0900

    (#104) fix : 로그인 오류 문제 해결

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,6 +4,7 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import KakaoProvider from "next-auth/providers/kakao";
 import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from 'bcrypt';
 
 export const authOptions = {
   providers: [
