6add3b1af45273ca8242bb46d65bc8a44c178228
hailey ham <roserealslow@gmail.com>
2024-03-31 11:53:39 +0900

    (#5) feat : 로그인-일반로그인

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,6 +3,7 @@ import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import KakaoProvider from "next-auth/providers/kakao";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
   providers: [
@@ -13,8 +14,53 @@ export const authOptions = {
     KakaoProvider({
       clientId: process.env.KAKAO_CLIENT_ID,
       clientSecret: process.env.KAKAO_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+
+      async authorize(credentials) {
+        let db = (await connectDB).db('study_platform');
+        let user = await db.collection('user_cred').findOne({ email: credentials.email });
+        if (!user) {
+          console.log('해당 이메일은 없음');
+          return null
+        }
+        const pwcheck = await bcrypt.compare(credentials.password, user.password);
+        if (!pwcheck) {
+          console.log('비번틀림');
+          return null
+        }
+        return user
+      }
     })
   ],
+
+  session: {
+    strategy: 'jwt',
+    maxAge: 30 * 24 * 60 * 60
+  },
+
+
+  callbacks: {
+    jwt: async ({ token, user }) => {
+      if (user) {
+        token.user = {};
+        token.user.name = user.name
+        token.user.email = user.email
+        token.user.image = user.image
+      }
+      return token;
+    },
+    session: async ({ session, token }) => {
+      session.user = token.user;
+      return session;
+    },
+  },
+
   secret: process.env.NEXTAUTH_JWT_SECRET,
   adapter: MongoDBAdapter(connectDB)
 };
