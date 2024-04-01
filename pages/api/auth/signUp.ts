b070f190ddd3c9ae9de2bde8ba6e951bf9066b80
hailey ham <roserealslow@gmail.com>
2024-04-01 15:28:44 +0900

    (#5) refac : 회원가입-이메일 중복 확인

--- pages/api/auth/signUp.ts
+++ pages/api/auth/signUp.ts
@@ -16,20 +16,29 @@ export default async function handler(
     if (req.body.info.pw == '') {
       return res.status(400).json('비밀번호를 입력해 주세요');
     }
-    try {
-      let userInfo = {
-        name: req.body.info.name,
-        email: req.body.info.email,
-        password: req.body.info.pw,
-        image: '/images/character/catBook.png',
-      };
-      const hash = await bcrypt.hash(req.body.info.pw, 10);
-      userInfo.password = hash;
-      const db = (await connectDB).db('study_platform');
-      await db.collection('user_cred').insertOne(userInfo);
-      return res.status(201).json('정상 요청');
-    } catch (error) {
-      return res.status(500).json('server error');
+
+    const db = (await connectDB).db('study_platform');
+    const findEmail = await db
+      .collection('user_cred')
+      .findOne({ email: req.body.info.email });
+
+    if (findEmail) {
+      return res.status(400).json('이미 가입된 이메일입니다.');
+    } else {
+      try {
+        let userInfo = {
+          name: req.body.info.name,
+          email: req.body.info.email,
+          password: req.body.info.pw,
+          image: '/images/character/catBook.png',
+        };
+        const hash = await bcrypt.hash(req.body.info.pw, 10);
+        userInfo.password = hash;
+        await db.collection('user_cred').insertOne(userInfo);
+        return res.status(201).json('정상 요청');
+      } catch (error) {
+        return res.status(500).json('server error');
+      }
     }
   } else {
     return res.status(400).json({ error: 'POST만 가능' });
