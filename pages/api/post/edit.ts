41e723b34544a80efb5db749a42cb1140825d6c3
hailey ham <roserealslow@gmail.com>
2024-04-01 18:55:14 +0900

    (#99) refac : 수정 서버-회원&작성자 확인 후 수정

--- pages/api/post/edit.ts
+++ pages/api/post/edit.ts
@@ -1,29 +1,48 @@
 import { connectDB } from '@/lib/utils/database';
 import { ObjectId } from 'mongodb';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../auth/[...nextauth]';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  try {
-    let editData = {
-      author: req.body.study.author,
-      title: req.body.study.title,
-      content: req.body.study.content,
-      status: req.body.study.status,
-      mode: req.body.study.mode,
-      location: req.body.study.location,
-      contact: req.body.study.contact,
-      date: req.body.study.date,
-      type: req.body.study.type,
-    };
+  if (req.method === 'POST') {
+    const session: any = await getServerSession(req, res, authOptions as any);
     const db = (await connectDB).db('study_platform');
-    const result = await db
+    const findPost = await db
       .collection('study_posts')
-      .updateOne({ _id: new ObjectId(req.body.study._id) }, { $set: editData });
-    return res.status(201).json('정상');
-  } catch (error) {
-    return res.status(500).json('서버 에러 발생');
+      .findOne({ _id: new ObjectId(req.body.study._id) });
+    console.log(findPost);
+
+    if (session) {
+      if (findPost.author === session.user.email) {
+        try {
+          let editData = {
+            author: req.body.study.author,
+            title: req.body.study.title,
+            content: req.body.study.content,
+            status: req.body.study.status,
+            mode: req.body.study.mode,
+            location: req.body.study.location,
+            contact: req.body.study.contact,
+            date: req.body.study.date,
+            type: req.body.study.type,
+          };
+          const result = await db
+            .collection('study_posts')
+            .updateOne(
+              { _id: new ObjectId(req.body.study._id) },
+              { $set: editData },
+            );
+          return res.status(201).json('정상');
+        } catch (error) {
+          return res.status(500).json('서버 에러 발생');
+        }
+      } else {
+        return res.status(500).json('글 작성자와 수정자가 불일치합니다.');
+      }
+    }
   }
 }
