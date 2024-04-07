62caf36d20a887a8c60fd9f93cf4ec9796111811
hailey ham <roserealslow@gmail.com>
2024-04-07 18:51:50 +0900

    (#110) feat: 퀴즈페이지 각 로딩

---
+++ app/quiz/play/loading.tsx
@@ -0,0 +1,12 @@
+import Loading from '@/components/loading/Loading';
+import React from 'react';
+
+export default function loading() {
+  return (
+    <div className="container">
+      <main>
+        <Loading></Loading>;
+      </main>
+    </div>
+  );
+}
