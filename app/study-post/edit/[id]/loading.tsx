acf9d24948deb71f1cd47a6504459a1d9f4b88f7
hailey ham <roserealslow@gmail.com>
2024-04-07 18:48:31 +0900

    (#110) design : 수정페이지 loading

--- app/study-post/edit/[id]/loading.tsx
+++ app/study-post/edit/[id]/loading.tsx
@@ -2,5 +2,13 @@ import Loading from '@/components/loading/Loading';
 import React from 'react';
 
 export default function loading() {
-  return <Loading></Loading>;
+  return (
+    <>
+      <div className="container" style={{ maxWidth: '800px' }}>
+        <main>
+          <Loading></Loading>;
+        </main>
+      </div>
+    </>
+  );
 }
