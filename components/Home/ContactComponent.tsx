5ba63da271b42aef951571157693673abc4481e0
hailey ham <roserealslow@gmail.com>
2024-04-13 03:37:02 +0900

    (#3) design : 메인페이지-카드 모바일 디자인 수정

--- components/Home/ContactComponent.tsx
+++ components/Home/ContactComponent.tsx
@@ -7,19 +7,19 @@ export default function ContactComponent() {
     <div className={styles.contactContainer}>
       <Link href="#">
         <img src="/images/character/blue.png" alt="" />
+        <p>공지</p>
+      </Link>
+      <Link href="#">
+        <img src="/images/character/rabbitBook.png" alt="" />
         <p>문의</p>
       </Link>
       <Link href="#">
-        <img src="/images/character/rabbitBook.png" alt="" />
-        <p>학습</p>
-      </Link>
-      <Link href="#">
         <img src="/images/character/catBook.png" alt="" />
-        <p>상담</p>
+        <p>뉴스</p>
       </Link>
       <Link href="#">
         <img src="/images/character/rabbitBook.png" alt="" />
-        <p>연락</p>
+        <p>이벤트</p>
       </Link>
     </div>
   );
