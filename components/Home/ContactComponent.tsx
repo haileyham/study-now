b8d54acaeada47b35488de3c20f7c41d9f0450d4
hailey ham <roserealslow@gmail.com>
2024-04-13 05:10:54 +0900

    (#3) design : 메인페이지-contact 캐릭터 추가 및 교체

--- components/Home/ContactComponent.tsx
+++ components/Home/ContactComponent.tsx
@@ -6,19 +6,19 @@ export default function ContactComponent() {
   return (
     <div className={styles.contactContainer}>
       <Link href="#">
-        <img src="/images/character/blue.png" alt="" />
+        <img src="/images/character/koala.png" alt="" />
         <p>공지</p>
       </Link>
       <Link href="#">
-        <img src="/images/character/rabbitBook.png" alt="" />
+        <img src="/images/character/dragonPhone.png" alt="" />
         <p>문의</p>
       </Link>
       <Link href="#">
-        <img src="/images/character/catBook.png" alt="" />
+        <img src="/images/character/pandaNews.png" alt="" />
         <p>뉴스</p>
       </Link>
       <Link href="#">
-        <img src="/images/character/rabbitBook.png" alt="" />
+        <img src="/images/character/mouseSign.png" alt="" />
         <p>이벤트</p>
       </Link>
     </div>
