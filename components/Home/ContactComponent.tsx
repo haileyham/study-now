a9b4f334c6bfe0e75e40048b58a22f34b72583cb
hailey ham <roserealslow@gmail.com>
2024-04-14 18:18:21 +0900

    (#117) 메인페이지-alt 추가

--- components/Home/ContactComponent.tsx
+++ components/Home/ContactComponent.tsx
@@ -6,19 +6,19 @@ export default function ContactComponent() {
   return (
     <div className={styles.contactContainer}>
       <Link href="/communications/notices">
-        <img src="/images/character/koala.png" alt="" />
+        <img src="/images/character/koala.png" alt="공지" />
         <p>공지</p>
       </Link>
       <Link href="/communications/inquiries">
-        <img src="/images/character/dragonPhone.png" alt="" />
+        <img src="/images/character/dragonPhone.png" alt="문의" />
         <p>문의</p>
       </Link>
       <Link href="/communications/news">
-        <img src="/images/character/pandaNews.png" alt="" />
+        <img src="/images/character/pandaNews.png" alt="뉴스" />
         <p>뉴스</p>
       </Link>
       <Link href="/communications/events">
-        <img src="/images/character/mouseSign.png" alt="" />
+        <img src="/images/character/mouseSign.png" alt="이벤트" />
         <p>이벤트</p>
       </Link>
     </div>
