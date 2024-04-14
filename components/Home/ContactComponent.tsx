8795cd1e3add1d7331da81603e2f874e73a84583
hailey ham <roserealslow@gmail.com>
2024-04-14 17:33:16 +0900

    (#117) feat:메인페이지-Contact 각 페이지 연결

--- components/Home/ContactComponent.tsx
+++ components/Home/ContactComponent.tsx
@@ -5,19 +5,19 @@ import React from 'react';
 export default function ContactComponent() {
   return (
     <div className={styles.contactContainer}>
-      <Link href="#">
+      <Link href="/communications/Notices">
         <img src="/images/character/koala.png" alt="" />
         <p>공지</p>
       </Link>
-      <Link href="#">
+      <Link href="/communications/inquiries">
         <img src="/images/character/dragonPhone.png" alt="" />
         <p>문의</p>
       </Link>
-      <Link href="#">
+      <Link href="/communications/news">
         <img src="/images/character/pandaNews.png" alt="" />
         <p>뉴스</p>
       </Link>
-      <Link href="#">
+      <Link href="/communications/events">
         <img src="/images/character/mouseSign.png" alt="" />
         <p>이벤트</p>
       </Link>
