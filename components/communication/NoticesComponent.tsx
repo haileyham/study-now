603e92213d4370b8065b20aa40f529406a9e03ad
hailey ham <roserealslow@gmail.com>
2024-04-15 17:21:31 +0900

    (#117) feat design : 공지페이지

--- components/communication/NoticesComponent.tsx
+++ components/communication/NoticesComponent.tsx
@@ -2,5 +2,27 @@ import styles from './page.module.scss';
 import React from 'react';
 
 export default function NoticesComponent() {
-  return <div>N</div>;
+  return (
+    <div className={styles.container}>
+      <header>
+        <h2>Notice</h2>
+      </header>
+      <section className={`${styles.notice} ${styles.board}`}>
+        <ul>
+          <li>
+            <p>New! 채용 정보 제공</p>
+          </li>
+          <li>
+            <p>스터디 검색 기능 추가</p>
+          </li>
+          <li>
+            <p>퀴즈 신규 업데이트</p>
+          </li>
+          <li>
+            <p>Study Now 출시!</p>
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
 }
