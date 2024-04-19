60539eb797c079e011dfbe416c13e0745dfb847c
hailey ham <roserealslow@gmail.com>
2024-04-19 13:35:26 +0900

    (#117) feat : 이벤트페이지-마크업 및 조건부렌더링 처리

--- components/communication/EventItemComponent.tsx
+++ components/communication/EventItemComponent.tsx
@@ -1,4 +1,5 @@
 'use client';
+import styles from './page.module.scss';
 import useIntersectionObserver from '@/hooks/useIntersectionObserver';
 import React from 'react';
 
@@ -6,13 +7,25 @@ interface EventItemType {
   image?: string;
   title: string;
   content?: string;
+  style?: string;
 }
 
 export default function EventItemComponent({
   image,
   title,
   content,
+  style,
 }: EventItemType) {
-  const { ref, isVisible } = useIntersectionObserver(0.3);
-  return <>{title}</>;
+  const { ref, isVisible } = useIntersectionObserver(0.5);
+  return (
+    <>
+      <div className={style}>
+        <div ref={ref} className={isVisible ? styles.visible : styles.hidden}>
+          <h3>{title}</h3>
+          <p>{content}</p>
+          <img src={image} alt="eventImg" />
+        </div>
+      </div>
+    </>
+  );
 }
