1b0dbfd354fd88f2a855eabea183437b54d57503
hailey ham <roserealslow@gmail.com>
2024-04-24 17:40:49 +0900

    (#3) feat : 메인페이지-footer

--- components/Home/Footer.tsx
+++ components/Home/Footer.tsx
@@ -1,11 +1,29 @@
+import Link from 'next/link';
 import styles from './page.module.scss';
 import React from 'react';
 
 export default function Footer() {
   return (
     <>
-      <div className={styles.container}>
-        
+      <div className={`${styles.container} ${styles.footer}`}>
+        <div className={styles.content}>
+          <ul>
+            <li>
+              <Link href="/communications/notices">공지사항</Link>
+            </li>
+            <li>
+              <Link href="#">이용약관</Link>
+            </li>
+            <li>
+              <Link href="#">개인정보처리방침</Link>
+            </li>
+          </ul>
+          <p>상업적 목적이 없는 개인 포트폴리오용 페이지입니다.</p>
+        </div>
+        <div className={styles.logo}>
+          <img src="/images/icons/studyNow.svg" alt="studyNow_logo" />
+          <p>ⓒ 2024 StudyNow. All rights reserved. </p>
+        </div>
       </div>
     </>
   );
