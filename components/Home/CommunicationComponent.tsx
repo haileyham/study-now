38bc5d2bd24158bc560dfbe1a7b0220ab56790a1
hailey ham <roserealslow@gmail.com>
2024-05-16 23:06:25 +0900

    (#137) refac : data 코드 분리 및 Image 최적화

--- components/Home/CommunicationComponent.tsx
+++ components/Home/CommunicationComponent.tsx
@@ -1,26 +1,31 @@
 import Link from 'next/link';
 import styles from './page.module.scss';
 import React from 'react';
+import getCommunicationData from './data/getCommunicationData';
+import Image from 'next/image';
 
 export default function CommunicationComponent() {
+  const communicationData = getCommunicationData();
   return (
     <div className={styles.communicationContainer}>
-      <Link href="/communications/notices">
-        <img src="/images/character/koala.png" alt="공지" />
-        <p>공지</p>
-      </Link>
-      <Link href="/communications/inquiries">
-        <img src="/images/character/dragonPhone.png" alt="문의" />
-        <p>문의</p>
-      </Link>
-      <Link href="/communications/news">
-        <img src="/images/character/pandaNews.png" alt="뉴스" />
-        <p>뉴스</p>
-      </Link>
-      <Link href="/communications/events">
-        <img src="/images/character/mouseSign.png" alt="이벤트" />
-        <p>이벤트</p>
-      </Link>
+      {communicationData.map((item, i) => {
+        return (
+          <Link href={item.link}>
+            <div className={styles.imageContainerCommu}>
+              <Image
+                src={item.img}
+                alt={item.alt}
+                layout="fill"
+                objectFit="cover"
+                quality={75}
+                placeholder="blur"
+                blurDataURL="/path/to/low-res-image.jpg"
+              />
+            </div>
+            <p>{item.title}</p>
+          </Link>
+        );
+      })}
     </div>
   );
 }
---
+++ components/Home/data/getCommunicationData.ts
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default function getCommunicationData() {
+  return [
+    {
+      link: '/communications/notices',
+      img: '/images/character/koala.png',
+      alt: '공지 이미지',
+      title: '공지',
+    },
+    {
+      link: '/communications/inquiries',
+      img: '/images/character/dragonPhone.png',
+      alt: '문의 이미지',
+      title: '문의',
+    },
+    {
+      link: '/communications/news',
+      img: '/images/character/pandaNews.png',
+      alt: '뉴스 이미지',
+      title: '뉴스',
+    },
+    {
+      link: '/communications/events',
+      img: '/images/character/mouseSign.png',
+      alt: '이벤트 이미지',
+      title: '이벤트',
+    },
+  ];
+}
